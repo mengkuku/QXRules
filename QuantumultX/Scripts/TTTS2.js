414017fc10eee97f07211f5885bd1f27a80648dd
mengkuku <36289945+mengkuku@users.noreply.github.com>
2025-08-21 14:38:42 +0800

    Create TTTS2.js

---
+++ QuantumultX/Scripts/TTTS2.js
@@ -0,0 +1,18 @@
+[rewrite_local]
+
+^https:\/\/api\.tiantiantiaosheng\.com\/\/api\/v1\/user\/entrance_info url script-response-body https://ghfast.top/raw.githubusercontent.com/mengkuku/QXRules/refs/heads/main/QuantumultX/Scripts/TTTS2.js
+
+[mitm] 
+
+hostname = api.tiantiantiaosheng.com
+
+
+
+*******************************/
+
+
+var objc = JSON.parse($response.body);
+
+    objc = 
+{"code":0,"msg":"success","data":{"member_status":"splus","entrance_ad":{"ad_text":"","ad_url":""},"wx_show":false,"wx_current_subscribe":false}}
+$done({body : JSON.stringify(objc)});
