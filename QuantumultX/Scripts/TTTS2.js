7dda5114705dfc791828e69dcd1df5e658618ce0
mengkuku <36289945+mengkuku@users.noreply.github.com>
2025-08-21 15:42:29 +0800

    Update TTTS2.js

--- QuantumultX/Scripts/TTTS2.js
+++ QuantumultX/Scripts/TTTS2.js
@@ -1,9 +1,19 @@
+
 [rewrite_local]
 
-^https:\/\/api\.tiantiantiaosheng\.com\/\/api\/v1\/user\/entrance_info url script-response-body local://k1.js
+^https:\/\/api\.tiantiantiaosheng\.com\/api2\/s_plus_member\/entrance_info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/TTTS.js
 
 [mitm] 
 
 hostname = api.tiantiantiaosheng.com
 
 
+
+*******************************/
+
+
+var objc = JSON.parse($response.body);
+
+    objc = 
+{"code":0,"msg":"success","data":{"member_status":"is_splus","entrance_ad":{"ad_text":"","ad_url":""},"wx_show":false,"wx_current_subscribe":false}}
+$done({body : JSON.stringify(objc)});
